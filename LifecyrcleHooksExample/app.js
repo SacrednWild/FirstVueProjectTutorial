77613d43cad766cf830d28538ff6ff81ab863bb5
Michele Rosito <githubmichelerosito@gmail.com>
2020-03-24 15:45:05 +0100

    Add created() to creat a fake card

--- LifecyrcleHooksExample/app.js
+++ LifecyrcleHooksExample/app.js
@@ -1,11 +1,23 @@
 const app = new Vue( {
   el: '#app',
   data: {
-    name: '',
-    email: '',
+    name: 'Loading...',
+    email: 'Loading...',
     company: {
-      name: '',
-      chatchPhrase: ''
+      name: 'Loading...',
+      chatchPhrase: 'Loading...'
     }
+  },
+  created() {
+    axios.get( 'https://jsonplaceholder.typicode.com/users' ).then( ( response ) => {
+      //Use response to update data
+      const data = response.data;
+      const randomUser = response.data[ Math.floor( Math.random() * data.length ) ];
+
+      this.name = randomUser.name;
+      this.email = randomUser.email;
+      this.company.name = randomUser.company.name;
+      this.company.chatchPhrase = randomUser.company.chatchPhrase;
+    } );
   }
 } );
