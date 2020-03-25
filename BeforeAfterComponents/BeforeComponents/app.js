4df79d304925b6d49457b277b5ef5150c263f047
Michele Rosito <githubmichelerosito@gmail.com>
2020-03-25 11:06:18 +0100

    Add component with a simple template

--- BeforeAfterComponents/BeforeComponents/app.js
+++ BeforeAfterComponents/BeforeComponents/app.js
@@ -30,3 +30,44 @@ const app = new Vue( {
   el:'#app',
   data: { tweets }
 } );
+
+const tweetComponent = Vue.component( 'tweet-component', {
+  //Options
+  template: `
+    <div v-for="tweet in tweets" v-bind:key="tweet.id" class="tweet">
+    <div class="box">
+      <article class="media">
+        <div class="media-left">
+          <figure class="image is-64x64">
+            <img v-bind:src="images/cry.jpg">
+          </figure>
+        </div>
+        <div class="media-content">
+          <div class="content">
+            <p>
+              <strong> James </strong>
+              <small> @jokerjames </small>
+              <br>
+              If you don't succeed, dust yourself off and try again.
+            </p>
+          </div>
+          <div class="level-left">
+            <a class="level-item">
+              <span class="icon is-small">
+                <i class="fas fa-heart"></i>
+              </span>
+              <span class="likes"> 10 </span>
+            </a>
+          </div>
+        </div>
+      </article>
+    </div>
+    <div class="control has-icons-left">
+      <input class="input is-small" placeholder="Tweet your reply..."/>
+      <span class="icon is-small is-left">
+        <i class="fas fa-envelope"></i>
+      </span>
+    </div>
+  </div>
+  `
+} )
