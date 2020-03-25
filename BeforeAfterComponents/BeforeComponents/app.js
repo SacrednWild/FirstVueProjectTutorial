9efdb289116a36e3011415e198e06480346ae38c
Michele Rosito <githubmichelerosito@gmail.com>
2020-03-25 09:18:16 +0100

    Add some tweets

---
+++ BeforeAfterComponents/BeforeComponents/app.js
@@ -0,0 +1,27 @@
+const tweets = 
+[ 
+  {
+    id: 1,
+    name: 'James',
+    handle: '@jokerjames',
+    img: 'images/cry.jpg',
+    tweet: 'If you don\'t succeed, dust yourself off and try again.',
+    likes: 10
+  },
+  {
+    id: 2,
+    name: 'Fatima',
+    handle: '@fantasticfatima',
+    img: 'images/girl.jpg',
+    tweet: 'Better late than never but never late is better.',
+    likes: 12
+  },
+  {
+    id: 3,
+    name: 'Xin',
+    handle: '@jokerjames',
+    img: 'images/manga.jpg',
+    tweet: 'Beauty in the struggle, ugliness in the success.',
+    likes: 10
+  }   
+]
